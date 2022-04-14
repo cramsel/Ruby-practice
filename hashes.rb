b8356affa3416538c6885cbf5b2f8b0372cc5139
cramsel <luxicorde@gmail.com>
2022-04-14 14:25:22 -0500

    Ruby Pract IC

---
+++ hashes.rb
@@ -0,0 +1,15 @@
+
+def store_person
+  person = {}
+  print "Enter first name: "
+  person["first_name"] = gets.chomp
+  print "Enter last name: "
+  person["last_name"] = gets.chomp
+  print "Enter email: "
+  person["email"] = gets.chomp
+  person.each do |k, v|
+    puts k[v]
+  end
+end
+
+store_person
