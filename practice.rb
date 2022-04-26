2aef97d9d4cd4036f56f95a753fea4820bec6bad
cramsel <luxicorde@gmail.com>
2022-04-25 19:46:09 -0500

    second bowling score

--- practice.rb
+++ practice.rb
@@ -1,8 +1,15 @@
-simple = [1,5, 4,4, 8,0, 5,3, 0,0, 4,5, 2,3, 8,1, 7,1, 1,1]
+simple = [2,5,  4,4,  8,'-',  5,3,  '-','-',  4,5,  2,3,  8,1,  7,1,  1,1]
 
 def scorekeep(array)
   score = 0
-  array.each {|number| score += number}
+  array.each do |number|
+    if number == '-'
+      points = 0
+    else
+      points = number
+    end 
+    score += points
+  end
   p score
 end
 
